a7ecf3e97071539675ca194b67b4b61ccacbb9aa
Johanna Palma <jnamla@gmail.com>
2019-09-28 07:57:09 +1000

    Sidenav and toolbar initial config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    loadChildren: './customers/customers.module#CustomersModule'
+  },
+  {
+    path: 'orders',
+    //loadChildren: './orders/orders.module#OrdersModule'
+    loadChildren: () => import('./orders/orders.module').then(mod => mod.OrdersModule) 
+  },
+  {
+    path: 'messages',
+    loadChildren: './messages/messages.module#MessagesModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
