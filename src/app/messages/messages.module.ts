435bfb68b54e2502e75702185645044522c1419a
Johanna Palma <jnamla@gmail.com>
2019-09-29 21:49:56 +1000

    Message stepper

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,13 +3,32 @@ import { CommonModule } from '@angular/common';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+//Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  declarations: [MessageListComponent],
+  declarations: [MessageListComponent, MessageNewComponent],
   imports: [
     CommonModule,
-    MessagesRoutingModule
+    MessagesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    //Material
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    MatIconModule,
+    MatAutocompleteModule
   ]
 })
 export class MessagesModule { }
