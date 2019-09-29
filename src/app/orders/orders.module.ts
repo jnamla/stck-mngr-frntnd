04e2013e6663180c14a0c3e0311e43a8658f4071
Johanna Palma <jnamla@gmail.com>
2019-09-30 02:19:53 +1000

    New order screen created

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderListComponent } from './order-list/order-list.component';
-import { MatTabsModule } from '@angular/material/tabs'
+import { MatTabsModule } from '@angular/material/tabs';
+import { OrderNewComponent } from './order-new/order-new.component'
 
 @NgModule({
-  declarations: [OrderListComponent],
+  declarations: [OrderListComponent, OrderNewComponent],
   imports: [
     CommonModule,
     OrdersRoutingModule,
