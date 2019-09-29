9e927e58d058e99b4f32ac822292f0c3dc274c45
Johanna Palma <jnamla@gmail.com>
2019-09-29 20:16:02 +1000

    Customer info - card

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerCardComponent } from '../customer-card/customer-card.component';
 
 @Component({
   selector: 'app-customer-list',
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+
+// Material Components
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    // Material components
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class CustomersModule { }
